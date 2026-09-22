5cea16a5c07dce54bb5596d430f7f7c8f299911f
IllusionAroundTheWorld <paraliwarrior@gmail.com>
2026-09-22 16:45:19 +0530

    Vercel.json remove - unwanted file

--- Frentend/src/App.js
+++ Frentend/src/App.js
@@ -6,7 +6,7 @@ import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import './App.css';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = `${process.env.REACT_APP_API_URL}/api/tasks`;
 
 function App() {
   const [tasks, setTasks] = useState([]);
